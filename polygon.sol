07a2a78543b7802ad85d6e142e7570ee7b1bb186
Andrew Kamal <kamalandrew55@gmail.com>
2023-02-17 22:33:07 -0500

    Create polygon.sol

---
+++ polygon.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.17;
+contract Polygon_Vault {
+    /**
+     * @dev will send to vault */  
+    string public DApp_URL = "PLEASE EDIT";
+    string public Network_Name = "PLEASE EDIT";
+    string public symbol = "PLEASE EDIT"; 
+    string public ERC20_Address = "PLEASE EDIT";
+    string public Map_TotalSupply = "PLEASE EDIT";
+}
