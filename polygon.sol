e17141514d2f3b3954199eb797e6b7e74ff06d72
Andrew Kamal <kamalandrew55@gmail.com>
2023-02-25 20:44:49 -0500

    Update polygon.sol

--- polygon.sol
+++ polygon.sol
@@ -2,9 +2,10 @@ pragma solidity ^0.8.17;
 contract Polygon_Vault {
     /**
      * @dev will send to vault */  
-    string public DApp_URL = "PLEASE EDIT";
-    string public Network_Name = "PLEASE EDIT";
-    string public symbol = "PLEASE EDIT"; 
-    string public ERC20_Address = "PLEASE EDIT";
-    string public Map_TotalSupply = "PLEASE EDIT";
+    string public DApp_URL = "https://app.superdao.co/cloutcontracts-dao";
+    string public Network_Name = "Polygon Network";
+    string public symbol = "CCSv2DAO"; 
+    string public ERC20_Address = "0xe346502D3BB39262EE2d9D2C52c7aE7C1f98E7e8";
+    string public Map_TotalSupply = "5000";
+    string public Map_TotalVal = "2000000 CCS";
 }
